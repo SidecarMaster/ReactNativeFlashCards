f926a4c4101d1f4d38118cdddaa1375ea1f058d8
Will Shu <will@Wills-iMac.local>
2017-10-15 22:21:22 +0800

    Move stack&tabNavigator into a separate file located in './screens' to handle only routing stuff.

--- App.js
+++ App.js
@@ -1,115 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions, TouchableHighlight } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import ReduxPromise from 'redux-promise'
 import ReduxThunk from 'redux-thunk'
-import { TabNavigator, StackNavigator } from 'react-navigation'
 import { Constants } from 'expo'
-import { Ionicons, FontAwesome } from '@expo/vector-icons'
 
+import { Router } from './src/screens'
 import reducer from './src/reducers'
-import { connectDeckList, connectAddDeck, connectDeckDetail, Quiz, connectAddCard } from './src/screens'
-import { BackButton } from './src/components/common'
-import { headerColor } from './src/utils/colors'
 import { setLocalNotification } from './src/utils/helpers'
-
-const SCREEN_HEIGHT = Dimensions.get('window').height
-const tabBarIconSize = 40
-
-const Home = TabNavigator({
-  DeckList: {
-    screen: connectDeckList,
-    navigationOptions: {
-      title: 'Home',
-      tabBarIcon: ({tintColor})=><Ionicons name='ios-home' size={tabBarIconSize} color={tintColor}/>,
-      headerStyle: {
-        backgroundColor: headerColor,
-      },
-      headerTitleStyle: {
-        color: 'white'
-      },
-    }
-  },
-  AddDeck: {
-    screen: connectAddDeck,
-    navigationOptions: ({navigation}) => ({
-      title: 'Add Deck',
-      tabBarIcon: ({tintColor})=><Ionicons name='ios-add' size={tabBarIconSize} color={tintColor} />,
-      headerStyle: {
-        backgroundColor: headerColor,
-      },
-      headerTitleStyle: {
-        color: 'white'
-      },
-      headerLeft: <BackButton navigation={navigation}/>
-    })
-  }
-},{
-  tabBarOptions: {
-    activeTintColor: '#01bdfa',
-    inactiveTintColor: '#6d8496',
-    style: {
-      backgroundColor: '#151c24',
-      height: SCREEN_HEIGHT*0.11,
-      paddingTop: 10,
-      paddingBottom: 6,
-    },
-    labelStyle: {
-      fontSize: 12
-    }
-  }
-})
-
-const Stacks = StackNavigator({
-  Home: {
-    screen: Home,
-  },
-  DeckDetail: {
-    screen: connectDeckDetail,
-    navigationOptions: ({navigation}) => ({
-      title: 'Deck Detail',
-      headerStyle: {
-        backgroundColor: headerColor,
-      },
-      headerTitleStyle: {
-        color: 'white'
-      },
-      headerLeft: <BackButton navigation={navigation}/>
-    })
-  },
-  AddCard: {
-    screen: connectAddCard,
-    navigationOptions: ({navigation}) => ({
-      title: 'Add Card',
-      headerStyle: {
-        backgroundColor: headerColor,
-      },
-      headerTitleStyle: {
-        color: 'white'
-      },
-      headerLeft: <BackButton navigation={navigation}/>
-    })
-  },
-  Quiz: {
-    screen: Quiz,
-    navigationOptions: ({navigation}) => ({
-      title: 'Quiz',
-      headerStyle: {
-        backgroundColor: headerColor,
-      },
-      headerTitleStyle: {
-        color: 'white'
-      },
-      headerLeft: <BackButton navigation={navigation}/>
-    })
-  },
-},{
-  cardStyle: {
-    backgroundColor: '#10131c'
-  },
-
-})
+import { headerColor } from './src/utils/colors'
 
 function AppStatusBar ({backgroundColor, ...props}) {
   return (
@@ -135,7 +35,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <View style={{flex:1}}>
           <AppStatusBar backgroundColor={headerColor} barStyle="light-content" />
-          <Stacks />
+          <Router />
         </View>
       </Provider>
     )
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,109 @@
-export * from './DeckList'
-export * from './DeckDetail'
-export * from './Quiz'
-export * from './AddDeck'
-export * from './AddCard'
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions, TouchableHighlight } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { Ionicons, FontAwesome } from '@expo/vector-icons'
+
+import { connectDeckList } from './DeckList'
+import { connectAddDeck } from './AddDeck'
+import { connectDeckDetail } from './DeckDetail'
+import { Quiz } from './Quiz'
+import { connectAddCard } from './AddCard'
+import { BackButton } from '../components/common'
+import { headerColor } from '../utils/colors'
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const tabBarIconSize = 40
+
+const Home = TabNavigator({
+  DeckList: {
+    screen: connectDeckList,
+    navigationOptions: {
+      title: 'Home',
+      tabBarIcon: ({tintColor})=><Ionicons name='ios-home' size={tabBarIconSize} color={tintColor}/>,
+      headerStyle: {
+        backgroundColor: headerColor,
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+    }
+  },
+  AddDeck: {
+    screen: connectAddDeck,
+    navigationOptions: ({navigation}) => ({
+      title: 'Add Deck',
+      tabBarIcon: ({tintColor})=><Ionicons name='ios-add' size={tabBarIconSize} color={tintColor} />,
+      headerStyle: {
+        backgroundColor: headerColor,
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerLeft: <BackButton navigation={navigation}/>
+    })
+  }
+},{
+  tabBarOptions: {
+    activeTintColor: '#01bdfa',
+    inactiveTintColor: '#6d8496',
+    style: {
+      backgroundColor: '#151c24',
+      height: SCREEN_HEIGHT*0.11,
+      paddingTop: 10,
+      paddingBottom: 6,
+    },
+    labelStyle: {
+      fontSize: 12
+    }
+  }
+})
+
+export const Router = StackNavigator({
+  Home: {
+    screen: Home,
+  },
+  DeckDetail: {
+    screen: connectDeckDetail,
+    navigationOptions: ({navigation}) => ({
+      title: 'Deck Detail',
+      headerStyle: {
+        backgroundColor: headerColor,
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerLeft: <BackButton navigation={navigation}/>
+    })
+  },
+  AddCard: {
+    screen: connectAddCard,
+    navigationOptions: ({navigation}) => ({
+      title: 'Add Card',
+      headerStyle: {
+        backgroundColor: headerColor,
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerLeft: <BackButton navigation={navigation}/>
+    })
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: ({navigation}) => ({
+      title: 'Quiz',
+      headerStyle: {
+        backgroundColor: headerColor,
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerLeft: <BackButton navigation={navigation}/>
+    })
+  },
+},{
+  cardStyle: {
+    backgroundColor: '#10131c'
+  },
+
+})
