c2dca4d06089dbaea6f7a2d5e6bc2421a4cc60b6
Will Shu <will@Wills-iMac.local>
2017-10-15 22:02:48 +0800

    Change reducers format to make it more readable

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,19 +12,35 @@ function storage(state={}, action) {
   switch (action.type) {
     case ALL_INFO:
       return action.payload
-    case ADD_DECK:
-      return {...state,
-        [action.payload]:{title:action.payload, questions:[]}
+    // You may wrap the whole thing after 'case ...:' in curly braces
+    case ADD_DECK: {
+      const { payload } = action
+      return {
+        ...state,
+        [payload]:{
+          title:payload,
+          questions:[]
+        }
       }
-    case ADD_CARD:
+    }
+    case ADD_CARD: {
       const { title, card: {question, answer} } = action.payload
-      return {...state,
-        [title]:{title, questions:[...state[`${title}`].questions, {question, answer}]}
+      return {
+        ...state,
+        [title]:{
+          title,
+          questions:[
+            ...state[`${title}`].questions,
+            {question, answer}
+          ]
+        }
       }
-    case DELETE_DECK:
+    }
+    case DELETE_DECK: {
       let newState={...state}
       delete newState[`${action.payload}`]
       return newState
+    }
     default:
       return state
   }
